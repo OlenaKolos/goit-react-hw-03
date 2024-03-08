6e746bb24877216a65baefcc1832f53fd7dd846c
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-03-08 21:44:18 +0200

    add change

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,54 @@
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import contactsData from "./components/data/contactsData.json";
 import "./App.css";
-import { FaBeer } from "react-icons/fa";
-// import Description from "./components/Description/Decscription";
-// import Options from "./components/Options/Options";
-// import Feedback from "./components/Feedback/Feedback";
-// import Notification from "./components/Notification/Notification";
 
 function App() {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem("contacts-list");
+    if (savedContacts !== null) {
+      try {
+        return JSON.parse(savedContacts);
+      } catch {
+        console.log("Error occured! Data removed!");
+      }
+    }
+    return contactsData;
+  });
+
+  const [filterValue, setFilter] = useState("");
+
+  useEffect(() => {
+    window.localStorage.setItem("contacts-list", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onAddContact = (values) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, values];
+    });
+  };
+
+  const onDeleteContact = (contactID) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter(({ id }) => id !== contactID);
+    });
+  };
+
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filterValue.toLowerCase())
+  );
+
   return (
     <>
-      <h3>
-        Lets go for a <FaBeer />?
-      </h3>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={onAddContact} />
+      <SearchBox filterValue={filterValue} setFilter={setFilter} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={onDeleteContact}
+      />
     </>
   );
 }
